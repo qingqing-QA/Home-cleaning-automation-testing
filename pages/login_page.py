1bff893b4d3b8ac55e371f2dbc77752c3b348636
qingqing-Na <narenbnu@gmail.com>
2026-03-27 22:28:30 -0400

    Implement login functionality in LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,1 +1,12 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
+class LoginPage(BasePage):
+    USERNAME = (By.ID, "username")
+    PASSWORD = (By.ID, "password")
+    LOGIN_BTN = (By.ID, "loginBtn")
+
+    def login(self, username, password):
+        self.send_keys(self.USERNAME, username)
+        self.send_keys(self.PASSWORD, password)
+        self.click(self.LOGIN_BTN)
